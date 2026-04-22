845ab30a0d18cff15b4462b8784598e405406627
Baldy76 <rgleech76@gmail.com>
2026-04-22 16:41:57 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-
-const CACHE_NAME = 'budget-v1';
+const CACHE_NAME = 'budget-v1.2';
 const ASSETS = [
   'index.html',
   'manifest.json'
@@ -7,6 +6,14 @@ const ASSETS = [
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+    })
+  );
 });
 
 self.addEventListener('fetch', (e) => {
