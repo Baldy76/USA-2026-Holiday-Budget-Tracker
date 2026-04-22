c66a75803f4d99e5bdcd7d5820c19173b35455ba
Baldy76 <rgleech76@gmail.com>
2026-04-22 16:53:12 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,16 @@
-const CACHE_NAME = 'budget-v1.4.1';
-const ASSETS = [
-  'index.html',
-  'manifest.json'
-];
+const CACHE_NAME = 'budget-v1.5.0';
+const ASSETS = ['index.html', 'manifest.json'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
-    })
-  );
+  e.waitUntil(caches.keys().then((keys) => {
+    return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)));
+  }));
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
